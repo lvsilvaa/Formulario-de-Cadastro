f8409ec129b22f8b9d6899d9ac533770a48d31fb
Leonardo Silva <leonardoo.silvaa2012@gmail.com>
2025-11-09 10:43:23 -0300

    Atualização de Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Cadastro } from './cadastro/cadastro';
 import { Consulta } from './consulta/consulta';
+import { Home } from './home/home';
 
 export const routes: Routes = [
+    {path: '', component: Home},
     {path: 'cadastro', component: Cadastro},
      {path: 'consulta', component: Consulta},
 ];
---
+++ src/app/home/home.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatIconModule}  from "@angular/material/icon"
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    MatIconModule
+  ],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+
+}
